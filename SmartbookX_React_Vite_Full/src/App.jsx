9093d543528a8d45dbae564731202f8647a9f1c1
smartbookx <smartbookx.app@gmail.com>
2025-07-11 08:33:57 +0530

    initial upload of smartbookx

---
+++ SmartbookX_React_Vite_Full/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to SmartbookX</h1>
+      <p>This is the full version demo built with React + Vite.</p>
+    </div>
+  );
+}
+
+export default App;
